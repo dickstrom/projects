bdf8a08b9eef0721c318f5bf54f7a578b0640d0a
dickstrom <83727995+dickstrom@users.noreply.github.com>
2021-08-06 23:18:40 +0200

    Initial commit

---
+++ MyBank/BankAccount.cs
@@ -0,0 +1,29 @@
+using System;
+namespace MyBank
+{
+    public class BankAccount
+    {
+        private static int accountNumberSeed = 1234567890;
+        public string Number { get; }
+        public string Owner { get; set; }
+        public string Balance { get; }
+        public void MakeDeposit(decimal amount, DateTime date, string note)
+        {
+            
+        }
+
+
+        public void MakeWithdrawal(decimal amount, DateTime date, string note)
+        {
+
+        }
+        public BankAccount(string name, decimal initialBalance)
+        {
+            this.Owner = name;
+            this.Balance = initialBalance.ToString();
+            this.Number = accountNumberSeed.ToString();
+            accountNumberSeed++;
+        }
+    }
+}
+    
---
+++ MyBank/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MyBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             var account = new BankAccount("Susans Money", 98988);
+    
+        Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance} dollars.");
+        }
+    }
+}
